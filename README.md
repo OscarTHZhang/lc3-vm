e287e5c626faf67e4954ec88d5ff06925c3dc2b8
Oscar Zhang <oscar.zth1999@gmail.com>
2020-08-08 19:43:55 -0500

    explored command line argument

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Little Computer 3 ISA - Architecture Simulator (Virtual Machine)
+# LC-3 Machine Simulator
+
+## Desgin
+
+Compile the simulator
+```
+cargo build
+```
+
+Run the machine simulator, given a specific lc3 trace file
+```
+cargo run trace/<lc3_trace_file>.asm
+```
+
+Run the simulator with debugging mode, given a specific lc3 trace file
+```
+cargo run --debugger trace/<lc3_trace_file>.asm
+```
+
+Run the visualizer, given a specific lc3 trace file
+```
+cargo run --visualizer trace/<lc3_trace_file>.asm
+```
 
 TODO Functionalities <br />
 * fully featured LC3 Architecture
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,30 @@ pub mod memory;
 pub mod register;
 pub mod instruction;
 
-fn main() -> std::io::Result<()> {
-    let path = std::env::current_dir()?;
-    println!("{}", path.display());
-    Ok(())
+use std::env;
+// use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    if args.len() <= 1 {
+        println!("[ERROR] Invalid number of arguments!");
+    } else if  args.len() == 2 {
+        println!("Run the simulator with a file provided...");
+        return;
+    } else if args.len() == 3 && args[1] == "debugger" {
+        println!("Run the simulator with debugging mode...");
+        return;
+    } else if args.len() == 3 && args[1] == "visualizer" {
+        println!("Run the simulaotr with WASM bindings for visualizer...");
+        return;
+    } else {
+        println!("[ERROR] Invalid number of arguments!");
+    }
 }
+
+// let file = &args[1];
+// let contents = fs::read_to_string(file)
+//     .expect("unable to open the file!");
+
+// println!("code:\n{} ", contents);
